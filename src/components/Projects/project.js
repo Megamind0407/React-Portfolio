4fd746f52d773f6c8f4d45fba567b8cdf7f0b250
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-06-02 16:35:56 +0530

    Changes in projects

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './project.css'
-
-
+import Img from "../../assets/fitlifehub.jpeg"
 const project = () => {
     return (
         <div className='projects'>
@@ -12,8 +11,21 @@ const project = () => {
                 <button className='btn1'>NextJs</button>
                 <button className='btn1'>MERN</button>
             </div>
+            <div className='project-card'>
+                <div className='content'>
+                    <img src={Img} alt=""></img>
+                </div>
+                <div className='project-title'>FitLife Hub</div>
+                <div className='btns'>
+                    <div className='bt1'><span className='text'>ReactJs</span></div>
+                    <div className='bt2'><span className='text'>Tailwind</span></div>
+                    <div className='bt3'><span className='text'>NodeJs</span></div>
+                </div>
+                <div className='demo'>
+                
+                </div>
+            </div>
         </div>
     )
 }
-
-export default project
+export default project;
