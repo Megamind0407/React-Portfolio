dc6d1dfc9a9a8e7d67bed8042b86dda2bcbfcdc8
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-07 02:04:37 +0530

    add new project

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -2,18 +2,15 @@ import React, { useState } from 'react';
 import './project.css';
 import Project1 from "../../assets/fitlifehub.jpeg";
 import Project2 from "../../assets/SkyEase Poster.png"
+import Project3 from '../../assets/expTracker.png'
 import { ExternalLink } from 'lucide-react';
 import { Code } from 'lucide-react';
 
 const Project = () => {
-    const [isVisible, setIsVisible] = useState(true);
+    const [isVisible, setIsVisible] = useState('All');
 
     const handleButtonClick = (buttonType) => {
-        if (buttonType === 'All'|| buttonType === 'ReactJs') {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
+        setIsVisible(buttonType);
     };
 
     return (
@@ -25,7 +22,7 @@ const Project = () => {
                 <button className='btn1' onClick={() => handleButtonClick('NextJs')}>NextJs</button>
                 <button className='btn1' onClick={() => handleButtonClick('MERN')}>MERN</button>
             </div>
-            {isVisible && (
+            {(isVisible === 'All' &&
                 <div className='project-container'>
                 <div className='project-card'>
                     <div className='content'>
@@ -93,22 +90,22 @@ const Project = () => {
                         </a>
                     </div>
                 </div>
-                {/* <div className='project-card'>
-                    <div className='content'>
-                        <img src={Project2} alt="Sky Ease" />
+                <div className='project-card'>
+                    <div className='poster'>
+                        <img src={Project3} alt="ExpTracker" />
                     </div>
-                    <div className='project-title'>SkyEase</div>
+                    <div className='project-title'>Expense Tracker</div>
                     <div className='desc'>
-                        <p>Established an Airline Flight Booking Website using ReactJs with efficient user experience</p>
+                        <p>Developed a full-stack web application called Expense Tracker using MERN stack.</p>
                     </div>
                     <div className='btns'>
-                        <div className='bt1'><span className='text'>HTML</span></div>
-                        <div className='bt3'><span className='text'>CSS</span></div>
                         <div className='bt4'><span className='text'>ReactJs</span></div>
                         <div className='bt4'><span className='text'>NodeJs</span></div>
+                        <div className='bt4'><span className='text'>MongoDB</span></div>
+                        <div className='bt4'><span className='text'>Express</span></div>
                     </div>
                     <div className='buttons'>
-                        <a href='https://sky-ease.vercel.app/' target='_blank' rel="noreferrer">
+                        <a href='https://expense-trackeret.vercel.app/' target='_blank' rel="noreferrer">
                             <button className='demo'>
                                 <div className='live'>
                                     <ExternalLink />
@@ -116,7 +113,44 @@ const Project = () => {
                                 <div className='btn-text'>Live</div>
                             </button>
                         </a>
-                        <a href='https://github.com/Megamind0407/SkyEase' target='_blank' rel="noreferrer">
+                        <a href='https://github.com/Megamind0407/expense-tracker' target='_blank' rel="noreferrer">
+                            <button className='code'>
+                                <div className='icon'>
+                                    <Code />
+                                </div>
+                                <div className='txt'>Code</div>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                </div>
+            )}
+            {(isVisible === 'ReactJs') && (
+                <div className='project-container'>
+                <div className='project-card'>
+                    <div className='content'>
+                        <img src={Project1} alt="FitLife Hub" />
+                    </div>
+                    <div className='project-title'>FitLife Hub</div>
+                    <div className='desc'>
+                        <p>Engineered a fitness website utilizing ReactJs with a seamless user interface</p>
+                    </div>
+                    <div className='btns'>
+                        <div className='bt1'><span className='text'>ReactJs</span></div>
+                        <div className='bt2'><span className='text'>Tailwind</span></div>
+                        <div className='bt3'><span className='text'>NodeJs</span></div>
+                        <div className='bt4'><span className='text'>Material UI</span></div>
+                    </div>
+                    <div className='buttons'>
+                        <a href='https://fitlifehub12.netlify.app/' target='_blank' rel="noreferrer">
+                            <button className='demo'>
+                                <div className='live'>
+                                    <ExternalLink />
+                                </div>
+                                <div className='btn-text'>Live</div>
+                            </button>
+                        </a>
+                        <a href='https://github.com/Megamind0407/fitlifehub.git' target='_blank' rel="noreferrer">
                             <button className='code'>
                                 <div className='icon'>
                                     <Code />
@@ -158,10 +192,47 @@ const Project = () => {
                             </button>
                         </a>
                     </div>
-                </div> */}
+                </div>
+                </div>
+                )}
+
+                {(isVisible === 'MERN') && (
+                <div className='project-container'>
+                <div className='project-card'>
+                    <div className='poster'>
+                        <img src={Project3} alt="ExpTracker" />
+                    </div>
+                    <div className='project-title'>Expense Tracker</div>
+                    <div className='desc'>
+                        <p>Developed a full-stack web application called Expense Tracker using MERN stack.</p>
+                    </div>
+                    <div className='btns'>
+                        <div className='bt4'><span className='text'>ReactJs</span></div>
+                        <div className='bt4'><span className='text'>NodeJs</span></div>
+                        <div className='bt4'><span className='text'>MongoDB</span></div>
+                        <div className='bt4'><span className='text'>Express</span></div>
+                    </div>
+                    <div className='buttons'>
+                        <a href='https://expense-trackeret.vercel.app/' target='_blank' rel="noreferrer">
+                            <button className='demo'>
+                                <div className='live'>
+                                    <ExternalLink />
+                                </div>
+                                <div className='btn-text'>Live</div>
+                            </button>
+                        </a>
+                        <a href='https://github.com/Megamind0407/expense-tracker' target='_blank' rel="noreferrer">
+                            <button className='code'>
+                                <div className='icon'>
+                                    <Code />
+                                </div>
+                                <div className='txt'>Code</div>
+                            </button>
+                        </a>
+                    </div>
+                </div>
                 </div>
             )}
-            
             {!isVisible && (
                 <div className='new_project' style={{textAlign:'center', marginTop:'5em',fontWeight:'bold'}}>New Projects Coming Soon.. 🧑‍💻</div>
             )}
